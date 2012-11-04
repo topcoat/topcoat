6f42c5e753d92e3db8e08d80b8ac8bf5a3023039
Brian LeRoux <brian@westcoastlogic.com>
2012-11-04 02:30:26 -0800

    tidy up teh gruntfile

--- grunt.js
+++ grunt.js
@@ -37,7 +37,8 @@ module.exports = function(grunt) {
             }
         }
     })
-  
+    
+    /* the manifest for component.json is used by Bower */  
     grunt.registerTask('manifest', 'Generates component.json file.', function() {
         fs.readdirSync(base).forEach(function(dir){
             if (dir != 'doc') {
