f02d56ae4d552abea2cbd1d5e74dcfe6917fd471
Brian LeRoux <brian@westcoastlogic.com>
2012-11-04 03:22:49 -0800

    added in Component package manager support

--- grunt.js
+++ grunt.js
@@ -1,6 +1,8 @@
 /*global module:false*/
 
-var component = { name: 'topcoat',version: '0.1.0', main: []}
+var bower     = { name: 'topcoat', version: '0.1.0', main: []}
+,   component = { repo: 'adobe/topcoat', description: 'An experimental CSS library.', styles:['release/css/topcoat-min.css'], files: []} 
+,   _         = require('underscore')
 ,   fs        = require('fs')
 ,   path      = require('path')
 ,   base      = path.join(__dirname, 'release')
@@ -41,14 +43,17 @@ module.exports = function(grunt) {
     /* the manifest for component.json is used by Bower */  
     grunt.registerTask('manifest', 'Generates component.json file.', function() {
         fs.readdirSync(base).forEach(function(dir){
-            if (dir != 'doc') {
-                var srcDir = path.join(base, dir)
-                fs.readdirSync(srcDir).forEach(function(srcFile) {
-                    component.main.push(path.join(dir, srcFile))
-                }) 
-            } 
+            var srcDir = path.join(base, dir)
+            fs.readdirSync(srcDir).forEach(function(srcFile) {
+                var srcFilePath = path.join(dir, srcFile)
+                // I do not understand why Bower requires a manifest if it relies on git solely. But whatever.
+                bower.main.push(srcFilePath)
+                // now adding fonts and images for Component
+                if (dir != 'css') component.files.push(srcFilePath)
+            }) 
         })
-        fs.writeFileSync(path.join(__dirname,'component.json'), JSON.stringify(component), 'utf8')
+        var c = JSON.stringify(_.extend(bower, component), null, 4)
+        fs.writeFileSync(path.join(__dirname,'component.json'), c, 'utf8')
     })
 
     // fin    
