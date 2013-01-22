2ccd3e4a19a1a10ad1218b083f05b4a597812697
kristoferjoseph <kristoferjoseph@gmail.com>
2013-01-22 15:16:44 -0800

    fixing lint errors in grunt file

--- grunt.js
+++ grunt.js
@@ -29,14 +29,14 @@ var bower = {
     _ = require('underscore'),
     fs = require('fs'),
     path = require('path'),
-    base = path.join(__dirname, 'release')
+    base = path.join(__dirname, 'release');
 
 
-    module.exports = function(grunt) {
+    module.exports = function (grunt) {
 
-        grunt.loadNpmTasks('grunt-contrib-stylus')
-        grunt.loadNpmTasks('grunt-contrib-mincss')
-        grunt.loadNpmTasks('grunt-contrib-copy')
+        grunt.loadNpmTasks('grunt-contrib-stylus');
+        grunt.loadNpmTasks('grunt-contrib-mincss');
+        grunt.loadNpmTasks('grunt-contrib-copy');
 
         grunt.initConfig({
             stylus: {
@@ -80,19 +80,19 @@ var bower = {
         /* the manifest for component.json is used by Bower */
         grunt.registerTask('manifest', 'Generates component.json file.', function() {
             fs.readdirSync(base).forEach(function(dir) {
-                var srcDir = path.join(base, dir)
+                var srcDir = path.join(base, dir);
                 fs.readdirSync(srcDir).forEach(function(srcFile) {
-                    var srcFilePath = path.join('release', dir, srcFile)
+                    var srcFilePath = path.join('release', dir, srcFile);
                     // I do not understand why Bower requires a manifest if it relies on git solely. But whatever.
-                    bower.main.push(srcFilePath)
+                    bower.main.push(srcFilePath);
                     // now adding fonts and images for Component
-                    if(dir != 'css') component.files.push(srcFilePath)
-                })
-            })
-            var c = JSON.stringify(_.extend(bower, component), null, 4)
-            fs.writeFileSync(path.join(__dirname, 'component.json'), c, 'utf8')
-        })
+                    if(dir != 'css') component.files.push(srcFilePath);
+                });
+            });
+            var c = JSON.stringify(_.extend(bower, component), null, 4);
+            fs.writeFileSync(path.join(__dirname, 'component.json'), c, 'utf8');
+        });
 
         // fin
-        grunt.registerTask('default', 'stylus copy mincss manifest')
-    }
+        grunt.registerTask('default', 'stylus copy mincss manifest');
+    };
