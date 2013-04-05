e8d8cc267975106160c1bc01888960396b08d5be
piatra <andrei.br92@gmail.com>
2013-04-05 18:30:59 +0300

    ability to submit test results on custom snapshots; related to #157

--- Gruntfile.js
+++ Gruntfile.js
@@ -233,13 +233,27 @@ module.exports = function(grunt) {
                 ,   test = grunt.option('test')
                 ,   host = process.env.TOPCOAT_BENCHMARK_SERVER
                 ,   port = process.env.TOPCOAT_BENCHMARK_PORT
+                ,   type = grunt.option('type')
+                ,   date = grunt.option('date')
+                ,   snapshot
                 ;
 
-                if (!path) {
-                    console.log('No path file specified');
-                    console.log('Usage: grunt telemetry-submit --path=path_to_output_file [--test= Test name ] [--device= Device type ]');
+                if (type && type.match(/snapshot/))
+                    snapshot = true; // we're dealing with a snapshot
+
+                if (!path || !type || (snapshot && !date)) {
+                    if (!path) console.log('No path file specified.');
+                    if (!type) console.log('Type not specified. Use --type=SHA or --type=snapshot');
+                    if (snapshot && !date) {
+                      console.log('You can use: date -u +"%Y-%m-%dT%H:%M:%SZ"');
+                      console.log('No date specified. Use --date=<date> in ISO standard with type snapshot');
+                    }
+                    grunt.fail.warn('Usage: grunt telemetry-submit --path=path_to_output_file --type=SHA|snapshot [--test= Test name ] [--device= Device type ]');
                 } else {
                     var submitData = require('./test/perf/telemetry/lib/submitData');
+                    if (snapshot)
+                        stdout = 'snapshot ' + date;
+
                     submitData(stdout, path, {
                         device: device,
                         test: test
