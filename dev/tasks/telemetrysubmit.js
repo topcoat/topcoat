2d2c4883258058198c352d66e27cdea0bd0480c7
piatra <andrei.br92@gmail.com>
2013-06-20 21:02:00 +0300

    updates to grunt telemetry-submit task

--- dev/tasks/telemetrysubmit.js
+++ dev/tasks/telemetrysubmit.js
@@ -6,40 +6,86 @@ module.exports = function(grunt) {
             commandToBeExecuted = 'git log --pretty=format:"%H %ci" | head -n 1',
             done = this.async();
 
-        exec(commandToBeExecuted, {cwd:gitCWD}, function(error, stdout, stderr) {
+        var part1 = {
+            properties: {
+                path: {
+                    message: 'Path to telemetry output file',
+                    required: true
+                },
+                device: {
+                    message: 'Device on which the test ran',
+                    required: true
+                }
+            }
+        };
+
+        var part2 = {
+            properties: {
+                test: {
+                    message: 'What is the name of the test?',
+                    required: true,
+                    default: ''
+                },
+                type: {
+                    message: 'Is it a test (nightly) ?',
+                    required: true,
+                    default: 'Yes'
+                }
+            }
+        };
+
+        exec(commandToBeExecuted, { cwd: gitCWD }, function(error, stdout, stderr) {
             if (error) {
                 grunt.log.error('Error');
                 console.log(error);
                 done();
-
             } else {
 
                 var path = grunt.option('path')
                 ,   device = grunt.option('device')
                 ,   test = grunt.option('test')
+                // use env variables to overwrite the location
+                // where the test results end up, for CI example
                 ,   host = process.env.TOPCOAT_BENCHMARK_SERVER
                 ,   port = process.env.TOPCOAT_BENCHMARK_PORT
                 ,   type = grunt.option('type')
                 ,   date = grunt.option('date')
                 ,   snapshot
+                ,   submitData = require('../test/perf/telemetry/lib/submitData')
+                ,   fileName = require('../test/perf/telemetry/lib/extractFileName')
+                ,   prompt = require('prompt')
                 ;
 
-                if (type && type.match(/snapshot/))
-                    snapshot = true; // we're dealing with a snapshot
+                if (!path || !test || !device) {
+                    // Dummy test to see if it was called without arguments
+                    // in this case we request the user for data
 
-                if (!path || !type || (snapshot && !date)) {
-                    if (!path) console.log('No path file specified.');
-                    if (!type) console.log('Type not specified. Use --type=SHA or --type=snapshot');
-                    if (snapshot && !date) {
-                      console.log('You can use: date -u +"%Y-%m-%dT%H:%M:%SZ"');
-                      console.log('No date specified. Use --date=<date> in ISO standard with type snapshot');
-                    }
-                    grunt.fail.warn('Usage: grunt telemetry-submit --path=path_to_output_file --type=SHA|snapshot [--test= Test name ] [--device= Device type ]');
+                    prompt.start();
+                    prompt.get(part1, function (err, result) {
+                        var options = result;
+
+                        part2.properties.test.default = fileName(result.path);
+
+                        prompt.get(part2, function (err, result) {
+                            for (var i in result)
+                                options[i] = result[i];
+
+                            if (options.type.match(/y/gi))
+                                options.date = 'snapshot ' + new Date().toISOString();
+                            else
+                                options.date = stdout;
+
+                            submitData(options.date, options.path, {
+                                device: options.device,
+                                test: options.test
+                            }, {
+                                host : host,
+                                port : port
+                            });
+                        });
+                    });
                 } else {
-                    var submitData = require('../test/perf/telemetry/lib/submitData');
-                    if (snapshot)
-                        stdout = 'snapshot ' + date;
-
+                    // This is in case for automated tasks that submits the data
                     submitData(stdout, path, {
                         device: device,
                         test: test
