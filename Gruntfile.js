dcd7d60b544e640ef5ca6e65206a7348c1154b17
kristoferjoseph <kristoferjoseph@gmail.com>
2013-10-18 17:10:17 -0700

    Updating for gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,7 +89,7 @@ module.exports = function(grunt) {
             usageguides: {
                 options: {
                     source: 'css',
-                    destination: 'demo',
+                    destination: '',
                     template: 'node_modules/topdoc-theme/',
                     templateData: {
                         "title": "Topcoat",
