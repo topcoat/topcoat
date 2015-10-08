90a4d8872dfebdee17ac6f54407ce7a1376a3287
garthdb <garthdb@gmail.com>
2015-10-08 11:15:04 -0700

    updated button-bar-base to npm build script

--- base/button-bar-base/test/button-bar.test.js
+++ base/button-bar-base/test/button-bar.test.js
@@ -1,12 +1,15 @@
 var assert = require('assert');
+var fs = require('fs');
+
+function read(file) {
+  return fs.readFileSync(file, 'utf8');
+}
 
 describe('button-bar-base', function() {
-    'use strict';
-
-    it('should create expected css file', function() {
-        var actual = fs.createReadStream('css/button-bar.css').trim();
-        var expected = fs.createReadStream('test/expected/button-bar.css').trim();
-        assert.equal(actual, expected, 'should generate correct css');
-    });
-
+  'use strict';
+  it('should create expected css file', function() {
+    var actual = read('css/button-bar.css').trim();
+    var expected = read('test/expected/button-bar.css').trim();
+    assert.equal(actual, expected, 'should generate correct css');
+  });
 });
