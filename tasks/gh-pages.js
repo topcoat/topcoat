b33c890fac9e5c0423bc02408c0480243ede9d57
Larry Davis <lawdavis@adobe.com>
2017-07-10 15:51:50 -0700

    DNA-287 Refactor to PostCSS implementation without mixins
    * Add gh-pages task

---
+++ tasks/gh-pages.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var git = require('gulp-git');
+var fs = require('fs');
+var package = JSON.parse(fs.readFileSync('./package.json'));
+
+gulp.task('gh-pages', function(cb) {
+  git.checkout('gh-pages', function(err) {
+    if (err) {
+      return cb(err);
+    }
+
+    // Move files
+    gulp.src('dist/**')
+      .pipe(gulp.dest('./'))
+      .on('end', function() {
+        // Commit
+        gulp.src([
+          './**',
+          '!./dist',
+          '!./dist/**',
+          '!./node_modules',
+          '!./node_modules/**'
+        ])
+          .pipe(git.commit('Deploy version ' + package.version))
+          .on('end', function() {
+            // Push
+            git.push('origin', 'gh-pages', function (err) {
+              if (err) {
+                return cb(err);
+              }
+
+              // Return to master
+              git.checkout('master', cb);
+            });
+          });
+      });
+  });
+});
